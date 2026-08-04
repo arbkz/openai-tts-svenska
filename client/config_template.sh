d6a7c8cf67982d307275481421d68e910a7bfb30
Arbkz <arbkz@users.noreply.github.com>
2026-08-04 20:40:47 +0200

    added new config template

---
+++ client/config_template.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# config.sh
+
+# set -x
+
+# Cloudflare Worker Config
+TTS_URL="https://your_tts_url/"
+WORKER_SECRET="__your_worker_secret"
+
+# TTS Settings
+VOICE="onyx"
+SPEED=1.0
+
+INSTRUCTIONS="Speak in natural Swedish with an authentic Scanian (Malmö) accent."
+
+# Shadowing
+PAUSE_MS=2000
+
+
+
+#!/usr/bin/env bash
