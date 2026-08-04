67eb4dd3a1282b90105b10469e049121bbe41831
Arbkz <arbkz@users.noreply.github.com>
2026-08-04 08:27:50 +0200

    Update submit_json_to_mp3.sh
    added config for and input output directory  
    write secret to variable before reading file directly in curl command

--- test dialogue/submit_json_to_mp3.sh
+++ test dialogue/submit_json_to_mp3.sh
@@ -6,8 +6,16 @@
 # set -x
 
 TTS_URL="https://openai-tts-svenska.christopher-barkhuizen.workers.dev/"
+
+INPUT_DIR="~/Downloads"
 INPUT_FILE=$1 
-WORKER_SECRET_FILE="worker_secret.txt"
+
+OUTPUT_DIR="~/svenska"
 OUTPUT_FILE=$(echo $1 | sed 's%json%mp3%' )
 
-curl -X POST $TTS_URL   -H "Authorization: Bearer $(cat $WORKER_SECRET_FILE)"   -H "Content-Type: application/json"   --data "@$INPUT_FILE"   --output "$OUTPUT_FILE"
+WORKER_SECRET_FILE="worker_secret.txt"
+WORKER_SECRET=$(cat $WORKER_SECRET_FILE)
+
+
+curl -X POST $TTS_URL   -H "Authorization: Bearer $WORKER_SECRET"   -H "Content-Type: application/json"   --data "@$INPUT_FILE"   --output "$OUTPUT_DIR/$OUTPUT_FILE"
+cp "$INPUT_DIR/$INPUT_FILE" "$OUTPUT_DIR/$OUTPUT_FILE"
